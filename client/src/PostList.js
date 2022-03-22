cedb885ee7f6f5fefcc82070a791fe25b003aa92
Felipe Belgine <felipe.belgine@capco.com>
2022-03-22 09:45:48 -0400

    add comment moderation and update event bus

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 
 export default () => {
     const [posts, setPosts] = useState({});
 
     const fetchPosts = async () => {
-        const res = await axios.get('http://localhost:4000/posts');
+        const res = await axios.get('http://localhost:4002/posts');
 
         setPosts(res.data);
     }
@@ -19,7 +20,8 @@ export default () => {
         return <div className="card" style={{width: '30%', marginBottom: '20px'}} key={post.id}>
             <div className="card-body">
                 <h3>{post.title}</h3>
-                <CommentCreate postId={post} />
+                <CommentList comments={post.comments}/>
+                <CommentCreate postId={post.id} />
             </div>
         </div>
     })
